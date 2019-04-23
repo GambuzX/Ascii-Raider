5e10da54757aa3e1cbb0e6c21132edf96b19f6e4
Mário Gil <mg_mesquita@hotmail.com>
2019-04-24 00:03:00 +0100

    Levels are now loaded from files

--- project/src/main/java/com/asciiraider/g710/Application.java
+++ project/src/main/java/com/asciiraider/g710/Application.java
@@ -4,10 +4,13 @@ import com.asciiraider.g710.model.level.*;
 
 public class Application {
     public static void main(String[] args) {
+        new Application().run();
+    }
+
+    private void run() {
         System.out.println("Hello World");
 
         LevelManager levelManager = LevelManager.getInstance();
         Level currentLevel = levelManager.nextLevel();
-
     }
 }
--- project/src/main/java/com/asciiraider/g710/model/level/LevelManager.java
+++ project/src/main/java/com/asciiraider/g710/model/level/LevelManager.java
@@ -1,5 +1,11 @@
 package com.asciiraider.g710.model.level;
 
+import com.asciiraider.g710.model.element.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -7,10 +13,10 @@ import java.util.ArrayList;
 public class LevelManager {
 
     private static LevelManager instance;
+    private final static int lvlCount = 2;
 
     private List<Level> levels = new ArrayList<>();
     private int currentLevel;
-    private LevelDirector levelDirector;
 
     public static LevelManager getInstance() {
         if (instance == null) {
@@ -34,19 +40,77 @@ public class LevelManager {
 
     private LevelManager() {
         currentLevel = 0;
-        levelDirector = new LevelDirector();
         buildAllLevels();
     }
 
     private void buildAllLevels() {
-        buildLevel(new LevelOneBuilder());
-        buildLevel(new LevelTwoBuilder());
+        for (int i = 1 ; i <= lvlCount; i++) {
+            try {
+                buildLevel(i);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private void buildLevel(LevelBuilder levelBuilder) throws IllegalArgumentException{
-        if (levelBuilder == null)
-            throw new IllegalArgumentException("Tried to use a null levelBuilder");
-        levelDirector.buildFullLevel(levelBuilder);
-        levels.add(levelBuilder.getResult());
+    private void buildLevel(int levelNumber) throws IOException {
+        List<String> levelLines = readLevelFile(levelNumber);
+        levels.add(buildLevelFromFile(levelLines));
+    }
+
+    private Level buildLevelFromFile(List<String> levelLines) {
+        Level newLevel = new Level();
+        for (int row = 0; row < levelLines.size(); row++) {
+            for (int col = 0; col < levelLines.get(0).length(); col++) {
+
+                char curr = levelLines.get(row).charAt(col);
+                Position pos = new Position(col, row);
+                Element newEle = null;
+
+                switch(curr) {
+                    case 'W':
+                        newEle = new Wall(pos);
+                        break;
+                    case 'S':
+                        newEle = new StoneBlock(pos);
+                        break;
+                    case 'B':
+                        newEle = new Boulder(pos);
+                        break;
+                    case 'P':
+                        newEle = new Player(pos);
+                        break;
+                    case 'E':
+                        newEle = new Enemy(pos);
+                        break;
+                    case 'T':
+                        newEle = new TNT(pos);
+                        break;
+                    case 's':
+                        newEle = new Sand(pos);
+                        break;
+                    case 'K':
+                        newEle = new LevelKey(pos);
+                        break;
+                    case 'D':
+                        newEle = new ExitDoor(pos);
+                        break;
+                }
+
+                if (newEle != null) newLevel.addElement(newEle);
+            }
+        }
+        return newLevel;
+    }
+
+    private List<String> readLevelFile(int levelNumber) throws IOException {
+        URL resource = LevelManager.class.getResource("/levels/" + levelNumber + ".lvl");
+        BufferedReader br = new BufferedReader(new FileReader(resource.getFile()));
+
+        List<String> lines = new ArrayList<>();
+        for (String line; (line = br.readLine()) != null; )
+            lines.add(line);
+
+        return lines;
     }
 }
