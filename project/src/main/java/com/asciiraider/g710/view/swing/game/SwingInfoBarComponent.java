d0054932517236edfd12c1b220b9e3779e1f11d8
GambuzX <mg_mesquita@hotmail.com>
2019-05-27 18:38:08 +0100

    temp commit to switch pc

--- project/src/main/java/com/asciiraider/g710/view/swing/game/SwingInfoBarComponent.java
+++ project/src/main/java/com/asciiraider/g710/view/swing/game/SwingInfoBarComponent.java
@@ -2,9 +2,13 @@ package com.asciiraider.g710.view.swing.game;
 
 import com.asciiraider.g710.GlobalConfigs;
 import com.asciiraider.g710.model.infobar.InfoBarModel;
+import com.asciiraider.g710.view.swing.menu.SwingMenuResources;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
 
 public class SwingInfoBarComponent extends JPanel {
@@ -18,12 +22,17 @@ public class SwingInfoBarComponent extends JPanel {
     private JLabel livesLabel;
     private JLabel rLabel;
 
+    BufferedImage background;
+
     public SwingInfoBarComponent() {
-        this.setLayout(new GridLayout(1, 18));
+        //this.setLayout(new GridLayout(1, 18));
+        this.setLayout(new GridBagLayout());
         this.setBackground(Color.GRAY);
 
         Font labelsFont = new Font("Serif", Font.PLAIN, GlobalConfigs.FONT_SIZE);
 
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.WEST;
         levelLabel = new JLabel();
         scoreLabel = new JLabel();
         keysProgressBar = new JProgressBar();
@@ -54,12 +63,42 @@ public class SwingInfoBarComponent extends JPanel {
         keysProgressBar.setFocusable(false);
         keysProgressBar.setStringPainted(true);
 
-        this.add(levelLabel);
-        this.add(scoreLabel);
-        this.add(keysProgressBar);
-        this.add(timeLabel);
-        this.add(livesLabel);
-        this.add(rLabel);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 0.5;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 4;
+        this.add(levelLabel, gbc);
+
+        gbc.gridx = 4;
+        gbc.gridwidth = 2;
+        this.add(scoreLabel, gbc);
+
+        gbc.gridx = 7;
+        gbc.gridwidth = 5;
+        this.add(keysProgressBar, gbc);
+
+        gbc.gridx = 12;
+        gbc.gridwidth = 2;
+        this.add(timeLabel, gbc);
+
+        gbc.gridx = 14;
+        gbc.gridwidth = 3;
+        this.add(livesLabel, gbc);
+
+        gbc.gridx = 20;
+        gbc.gridwidth = 1;
+        this.add(rLabel, gbc);
+
+        try {
+            background = ImageIO.read(SwingInfoBarComponent.class.getResource("/symbols/infobar_background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void setInfoBarModel(InfoBarModel infoBarModel) {
@@ -74,6 +113,8 @@ public class SwingInfoBarComponent extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
+
+        graphics.drawImage(background, 0, 0, null);
 
         if (infoBarModel == null) return;
 
