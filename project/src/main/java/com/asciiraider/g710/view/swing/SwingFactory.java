5bd7415f29dc1a5f96b898197e73118c63b1a475
Mário Gil <mg_mesquita@hotmail.com>
2019-05-30 11:19:47 +0100

    Resources Manager class

--- project/src/main/java/com/asciiraider/g710/view/swing/SwingFactory.java
+++ project/src/main/java/com/asciiraider/g710/view/swing/SwingFactory.java
@@ -14,24 +14,26 @@ import java.awt.*;
 
 public class SwingFactory implements ViewFactory {
     private JFrame frame;
+    private SwingResourceManager resourceManager;
 
     public SwingFactory() {
         frame = new JFrame(GlobalConfigs.GAME_NAME);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new MigLayout("wrap 1, insets 0 0, gap 0 0, fill, align 0% 0%"));
         frame.setSize(new Dimension(GlobalConfigs.LEVEL_WIDTH * GlobalConfigs.SWING_SIZE_FACTOR, (GlobalConfigs.LEVEL_HEIGHT + GlobalConfigs.INFOBAR_HEIGHT) * GlobalConfigs.SWING_SIZE_FACTOR));
+        resourceManager = new SwingResourceManager();
     }
 
     @Override
     public SwingStateView<MenuModel> createMenuView() {
         frame.getContentPane().removeAll();
-        return new SwingMenuView(frame);
+        return new SwingMenuView(frame, resourceManager);
     }
 
     @Override
     public SwingStateView<LevelModelGroup> createLevelView() {
         frame.getContentPane().removeAll();
-        return new SwingGroupLevelView(frame);
+        return new SwingGroupLevelView(frame, resourceManager);
     }
 
     @Override
