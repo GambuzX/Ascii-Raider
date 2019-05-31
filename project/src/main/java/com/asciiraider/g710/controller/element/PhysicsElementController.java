d2cf805ac624365152224fb9f34e91cd90c76898
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-05-31 09:45:24 +0100

    Fixed explosion bug

--- project/src/main/java/com/asciiraider/g710/controller/element/PhysicsElementController.java
+++ project/src/main/java/com/asciiraider/g710/controller/element/PhysicsElementController.java
@@ -17,13 +17,14 @@ public class PhysicsElementController {
 
 	public void handleElementPhysics(LevelController levelController, LevelFacade levelFacade) {
 		Position nextPosition = physicsElement.moveDown();
-		levelFacade.setElementPosition(physicsElement, nextPosition);
 
 		Element belowEle = levelFacade.findElement(physicsElement.getPosition().getBelow());
 
 
-		if (belowEle == null )
+		if (belowEle == null ) {
 			physicsElement.setFalling(true);
+			levelFacade.setElementPosition(physicsElement, nextPosition);
+		}
 		// TODO: tirar isto para fora
 		else{
 			if (physicsElement instanceof Explosive && physicsElement.isFalling()) {
